b48e280c2f622e523340d801d22e54412a909f71
Kerolos Emad <58334774+kerolos350@users.noreply.github.com>
2023-09-18 08:04:59 +0300

    nothing

--- JS/tasksCheck.js
+++ JS/tasksCheck.js
@@ -3,11 +3,11 @@ function checkAll () {
     var otherCheck = document.getElementsByClassName("tChk");
 
     if (main.checked == true){
-        for (let index = 0; index <= otherCheck.length; index++) {
+        for (let index = 0; index < otherCheck.length; index++) {
             otherCheck[index].checked = true;
         }
     } else {
-        for (let index = 0; index <= otherCheck.length; index++) {
+        for (let index = 0; index < otherCheck.length; index++) {
             otherCheck[index].checked = false;
         }
     }
